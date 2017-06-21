1c73927761ff8e446e4c46e010ba538706d2a02b
pstefanm <p.stefanm@ymail.com>
2017-06-21 17:42:17 +0300

    finished ep2

--- src/com/tube/rest/status/V1_status.java
+++ src/com/tube/rest/status/V1_status.java
@@ -1,15 +1,25 @@
 package com.tube.rest.status;
 
 import javax.ws.rs.*;
-import javax.ws.rs.core.MediaType;;
+import javax.ws.rs.core.MediaType;
 
-@Path("/v1 status")
+@Path("/v1")
 public class V1_status {
 
+	private static final String api_version= "00.10.00";
+	
+	@Path("/status")
 	@GET
 	@Produces(MediaType.TEXT_HTML)
-	public String returnTitle(){
-		return "<p>Java Web Service</p>";
+	public String returnTitle() {
+		return "<p>Java Web Service Status</p>";
 	}
 	
+	@Path("/version")
+	@GET
+	@Produces(MediaType.TEXT_HTML)
+	public String returnVersion() {
+		return "<p>Java Web Service Version: </p>" + api_version;
+	}
+
 }
