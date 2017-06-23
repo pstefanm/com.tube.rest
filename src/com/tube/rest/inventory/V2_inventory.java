0186df1e2bb93d66d4e465713c6fdc7e3727a0b8
pstefanm <p.stefanm@ymail.com>
2017-06-23 17:02:38 +0300

    Added an example of Path Param
    Added an example of brand Path parameter beside the brand query parameter

--- src/com/tube/rest/inventory/V2_inventory.java
+++ src/com/tube/rest/inventory/V2_inventory.java
@@ -2,6 +2,7 @@ package com.tube.rest.inventory;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
@@ -18,7 +19,6 @@ public class V2_inventory {
 	@Produces(MediaType.APPLICATION_JSON)
 	public Response returnBrandParts(@QueryParam("brand") String brand) throws Exception {
 
-		Response response = null;
 		JSONArray jArray = new JSONArray();
 		String returnString = null;
 
@@ -26,6 +26,40 @@ public class V2_inventory {
 
 			if (brand == null) {
 				return Response.status(400).entity("ERROR: Please specify the brand for this search").build();
+			}
+			SchemaSql sql = new SchemaSql();
+			jArray = sql.returnPartBrands(brand);
+
+			returnString = jArray.toString();
+
+		} catch (Exception e) {
+			e.printStackTrace();
+			return Response.status(500).entity("Server was not able to process your request").build();
+		}
+
+		return Response.ok(returnString).build();
+
+	}
+/*
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response returnErrorOnBrand() throws Exception {
+
+		return Response.status(400).entity("Error: Cannot process: no brand mentione").build();
+	}
+*/
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	@Path("/{brand}")
+	public Response returnBrand(@PathParam("brand") String brand) throws Exception {
+
+		JSONArray jArray = new JSONArray();
+		String returnString = null;
+
+		try {
+
+			if (brand == null) {
+				return Response.status(400).entity("ERROR: Server was not able to process your request").build();
 			}
 			SchemaSql sql = new SchemaSql();
 			jArray = sql.returnPartBrands(brand);
