bbf53335488d58603c05e8b96126873212dfc52a
pstefanm <p.stefanm@ymail.com>
2017-06-27 16:34:56 +0300

    Added add part functionality
    Added functionality to add part via Jackson ObjectMapper:
    Added: post.html, post.js,jquery-min.js, method in V2_inventory, method in SchemaSql, class in V2_inventory.

--- src/com/tube/rest/inventory/V2_inventory.java
+++ src/com/tube/rest/inventory/V2_inventory.java
@@ -1,6 +1,8 @@
 package com.tube.rest.inventory;
 
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -8,6 +10,7 @@ import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
+import org.codehaus.jackson.map.ObjectMapper;
 import org.codehaus.jettison.json.JSONArray;
 
 import com.tube.rest.dao.SchemaSql;
@@ -101,5 +104,49 @@ public class V2_inventory {
 		return Response.ok(returnString).build();
 
 	}
+	
+	@POST
+	@Consumes({MediaType.APPLICATION_FORM_URLENCODED, MediaType.APPLICATION_JSON})
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response addPcParts(String incomingData) throws Exception {
+		
+		//JSONArray jArray = new JSONArray();
+		String returnString = null;
+		
+		try {
+			
+			System.out.println("Incoming Data: " + incomingData);
+			
+			SchemaSql sql = new SchemaSql();
+			ObjectMapper mapper = new ObjectMapper();
+			
+			ItemEntry itemEntry = mapper.readValue(incomingData, ItemEntry.class);
+			
+			int http_code = sql.insertIntoPC_PARTS(itemEntry.PC_PARTS_TITLE,
+													itemEntry.PC_PARTS_CODE,
+													itemEntry.PC_PARTS_MAKER,
+													itemEntry.PC_PARTS_AVAIL,
+													itemEntry.PC_PARTS_DESC);
+			
+			if (http_code == 200) {
+				// returnString = jArray.toString();
+				returnString = "Item Inserted.";
+			}else {
+				return Response.status(500).entity("Unable to process Item").build();
+			}
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+			return Response.status(500).entity("Server was not able to process your request").build();
+		}
+		
+		return Response.ok(returnString).build();
+	}
 
+}
+
+class ItemEntry{
+	
+	public String PC_PARTS_TITLE, PC_PARTS_CODE, PC_PARTS_MAKER, PC_PARTS_AVAIL, PC_PARTS_DESC;
+	
 }
